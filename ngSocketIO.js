e241bd4a381332a0f3fe3c75f11ae5945ab88990
Michael Benford <michael@michaelbenford.net>
2013-06-28 01:02:39 -0300

    Added removeAllListeners and addListenerOnce

--- ngSocketIO.js
+++ ngSocketIO.js
@@ -3,40 +3,43 @@
 angular.module('ngSocketIO', []).factory('socket', function($rootScope) {
     var socket = io.connect();
 
-    var on = function(name, callback) {
-        socket.addListener(name, function() {
-            var args = arguments;
-            $rootScope.$apply(function() {
-                callback.apply(socket, args);
-            });
-        });
-    };
-
-    var off = function(name, callback) {
-        socket.removeListener(name, function() {
-            var args = arguments;
-            $rootScope.$apply(function() {
-                callback.apply(socket, args);
-            });
-        });
-    };
-
-    var emit = function(name, data, callback) {
-        socket.emit(name, data, function () {
+    var angularCallback = function(callback) {
+        return function() {
             if (callback) {
                 var args = arguments;
-                $rootScope.$apply(function () {
+                $rootScope.$apply(function() {
                     callback.apply(socket, args);
                 });
             }
-        })
+        }
+    };
+
+    var addListener = function(name, callback) {
+        socket.addListener(name, angularCallback(callback));
+    };
+
+    var addListenerOnce = function(name, callback) {
+        socket.once(name, angularCallback(callback));
+    };
+
+    var removeListener = function(name, callback) {
+        socket.removeListener(name, angularCallback(callback));
+    };
+
+    var removeAllListeners = function(name) {
+        socket.removeAllListeners(name);
+    };
+
+    var emit = function(name, data, callback) {
+        socket.emit(name, data, angularCallback(callback));
     };
 
     return {
-        on: on,
-        addListener: on,
-        off: off,
-        removeListener: off,
+        addListener: addListener,
+        on: addListener,
+        once: addListenerOnce,
+        removeListener: removeListener,
+        removeAllListeners: removeAllListeners,
         emit: emit
     };
 });
